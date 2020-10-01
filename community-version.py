86c8080db24bfc2df8558e635ee6554458a26353
Mehul Gupta <70238140+MehulGupta21@users.noreply.github.com>
2020-10-02 05:16:43 +0530

    Update Community-version.py

--- community-version.py
+++ community-version.py
@@ -3,6 +3,7 @@
 #code credit goes to: https://www.hackerearth.com/practice/notes/beautiful-python-a-simple-ascii-art-generator-from-images/
 #code modified to work with Python 3 by @aneagoie
 from PIL import Image
+example/make_art.py/ztm-logo.png
 ASCII_CHARS = [ '#', '?', '%', '.', 'S', '+', '.', '*', ':', ',', '@']
 
 def scale_image(image, new_width=100):
