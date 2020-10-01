1c7a97099e891a6b5b20c20145c42c7261e598ad
aapetsi <apetsiampiah@gmail.com>
2020-10-01 16:53:36 +0000

    fix py2 file to run with py3

--- example/make_art_python2.py
+++ example/make_art_python2.py
@@ -26,7 +26,7 @@ def map_pixels_to_ascii_chars(image, range_width=25):
     """
 
     pixels_in_image = list(image.getdata())
-    pixels_to_chars = [ASCII_CHARS[pixel_value/range_width] for pixel_value in
+    pixels_to_chars = [ASCII_CHARS[pixel_value//range_width] for pixel_value in
             pixels_in_image]
 
     return "".join(pixels_to_chars)
@@ -39,7 +39,7 @@ def convert_image_to_ascii(image, new_width=100):
     len_pixels_to_chars = len(pixels_to_chars)
 
     image_ascii = [pixels_to_chars[index: index + new_width] for index in
-            xrange(0, len_pixels_to_chars, new_width)]
+            range(0, len_pixels_to_chars, new_width)]
 
     return "\n".join(image_ascii)
 
@@ -47,13 +47,13 @@ def handle_image_conversion(image_filepath):
     image = None
     try:
         image = Image.open(image_filepath)
-    except Exception, e:
-        print "Unable to open image file {image_filepath}.".format(image_filepath=image_filepath)
-        print e
+    except Exception as e:
+        print("Unable to open image file {image_filepath}.".format(image_filepath=image_filepath))
+        print(e)
         return
 
     image_ascii = convert_image_to_ascii(image)
-    print image_ascii
+    print(image_ascii)
 
 if __name__=='__main__':
     import sys
