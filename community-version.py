c7745710ebeca77a95be0bd9b3dc806d820b98f1
adamstok <adamstokowski@outlook.com>
2020-10-01 19:38:06 +0200

    arguments

--- community-version.py
+++ community-version.py
@@ -1,1 +1,81 @@
+# this project requires Pillow installation: https://pillow.readthedocs.io/en/stable/installation.html
 
+#code credit goes to: https://www.hackerearth.com/practice/notes/beautiful-python-a-simple-ascii-art-generator-from-images/
+#code modified to work with Python 3 by @aneagoie
+from PIL import Image
+
+help_msg = """
+Usage  : python community-version.py [option] [input_file]
+Options:
+    -r   reverse the ASCII_CHARS
+    -s   save the output to file (by default the output file is output_[input_file].txt)
+"""
+
+
+def scale_image(image, new_width=100):
+    """Resizes an image preserving the aspect ratio.
+    """
+    (original_width, original_height) = image.size
+    aspect_ratio = original_height/float(original_width)
+    new_height = int(aspect_ratio * new_width)
+
+    new_image = image.resize((new_width, new_height))
+    return new_image
+
+def convert_to_grayscale(image):
+    return image.convert('L')
+
+def map_pixels_to_ascii_chars(image, range_width=25):
+    """Maps each pixel to an ascii char based on the range
+    in which it lies.
+
+    0-255 is divided into 11 ranges of 25 pixels each.
+    """
+
+    pixels_in_image = list(image.getdata())
+    pixels_to_chars = [ASCII_CHARS[int(pixel_value/range_width)] for pixel_value in
+            pixels_in_image]
+
+    return "".join(pixels_to_chars)
+
+def convert_image_to_ascii(image, new_width=100):
+    image = scale_image(image)
+    image = convert_to_grayscale(image)
+
+    pixels_to_chars = map_pixels_to_ascii_chars(image)
+    len_pixels_to_chars = len(pixels_to_chars)
+
+    image_ascii = [pixels_to_chars[index: index + new_width] for index in
+            range(0, len_pixels_to_chars, new_width)]
+
+    return "\n".join(image_ascii)
+
+def handle_image_conversion(image_filepath):
+    image = None
+    try:
+        image = Image.open(image_filepath)
+    except Exception as e:
+        print(f"Unable to open image file {image_filepath}.")
+        print(e)
+        return
+
+    image_ascii = convert_image_to_ascii(image)
+    print(image_ascii)
+
+def check_inputs():
+    allowed_inputs_file = ["png"]
+    arguments = [x for x in sys.arg]
+
+if __name__=='__main__':
+    import sys
+    # allowed_inputs_file = ["png"]
+    todo = check_inputs()
+    arguments = [x for x in sys.argv]
+    if '-r' in arguments:
+        ASCII_CHARS = [ '#', '?', '%', '.', 'S', '+', '.', '*', ':', ',', '@'][::-1]
+    else:
+        ASCII_CHARS = [ '#', '?', '%', '.', 'S', '+', '.', '*', ':', ',', '@']
+
+    image_file_path = sys.argv[1]
+    print(image_file_path)
+    handle_image_conversion(image_file_path)
