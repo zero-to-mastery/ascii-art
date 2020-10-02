638eb253eab693a5ebd3fa309fd2a27ff934c2c1
Siddharth2016 <siddharthchandragzb@gmail.com>
2020-10-02 19:17:33 +0530

    create ConvertImageToASCII, refactor code to OO

--- community-version.py
+++ community-version.py
@@ -1,9 +1,17 @@
 # this project requires Pillow installation: https://pillow.readthedocs.io/en/stable/installation.html
 #code credit goes to: https://www.hackerearth.com/practice/notes/beautiful-python-a-simple-ascii-art-generator-from-images/
 #code modified to work with Python 3 by @aneagoie
+
+import os
+import sys
 from PIL import Image
 
-help_msg = """
+# Global Variables
+OUT_FILE_PATH = "./output_images/"
+
+ASCII_CHARS = [ '#', '?', '%', '.', 'S', '+', '.', '*', ':', ',', '@']
+
+HELP_MSG = """
 Usage  : python community-version.py [option] [input_file]
 Options:
          no options will run the default ASCII_CHARS
@@ -12,82 +20,110 @@ Options:
     -rs  save the reversed output to file
 """
 
+FILE_ERROR_MSG = """
+File type of [input_file] not valid, please make sure it is of type PNG.
+"""
 
-def scale_image(image, new_width=100):
-    """Resizes an image preserving the aspect ratio.
-    """
-    (original_width, original_height) = image.size
-    aspect_ratio = original_height/float(original_width)
-    new_height = int(aspect_ratio * new_width)
 
-    new_image = image.resize((new_width, new_height))
-    return new_image
+class ConvertImageToASCII():
 
-def convert_to_grayscale(image):
-    return image.convert('L')
+    def __init__(self, file_path=None, option='-c'):
+        ''' Default file_path is None and option to use is -c '''
+        self.image_file_path = file_path
+        self.option = option
 
-def map_pixels_to_ascii_chars(image, range_width=25):
-    """Maps each pixel to an ascii char based on the range
-    in which it lies.
-    0-255 is divided into 11 ranges of 25 pixels each.
-    """
+    def scale_image(self, image, new_width=100):
+        """Resizes an image preserving the aspect ratio.
+        """
+        (original_width, original_height) = image.size
+        aspect_ratio = original_height/float(original_width)
+        new_height = int(aspect_ratio * new_width)
+        new_image = image.resize((new_width, new_height))
+        return new_image
 
-    pixels_in_image = list(image.getdata())
-    pixels_to_chars = [ASCII_CHARS[int(pixel_value/range_width)] for pixel_value in
-            pixels_in_image]
+    def convert_to_grayscale(self, image):
+        return image.convert('L')
+    
+    def map_pixels_to_ascii_chars(self, image, range_width=25):
+        """Maps each pixel to an ascii char based on the range
+        in which it lies.
 
-    return "".join(pixels_to_chars)
+        0-255 is divided into 11 ranges of 25 pixels each.
+        """
+        global ASCII_CHARS
+        pixels_in_image = list(image.getdata())
+        pixels_to_chars = [ASCII_CHARS[int(pixel_value/range_width)] for pixel_value in
+                pixels_in_image]
 
-def convert_image_to_ascii(image, new_width=100):
-    image = scale_image(image)
-    image = convert_to_grayscale(image)
+        return "".join(pixels_to_chars)
 
-    pixels_to_chars = map_pixels_to_ascii_chars(image)
-    len_pixels_to_chars = len(pixels_to_chars)
+    def convert_image_to_ascii(self, image, new_width=100):
+        image = self.scale_image(image)
+        image = self.convert_to_grayscale(image)
 
-    image_ascii = [pixels_to_chars[index: index + new_width] for index in
-            range(0, len_pixels_to_chars, new_width)]
+        pixels_to_chars = self.map_pixels_to_ascii_chars(image)
+        len_pixels_to_chars = len(pixels_to_chars)
 
-    return "\n".join(image_ascii)
+        image_ascii = [pixels_to_chars[index: index + new_width] for index in
+                range(0, len_pixels_to_chars, new_width)]
+        return "\n".join(image_ascii)
 
-def write_image_to_text_file(image_ascii):
+    def map_option_to_convert(self, image):
+        ''' Make to appropriate option provided to convert image '''
+        converted_image = self._defaultConversion(image)
+        if 'r' in self.option:
+            converted_image = self._reverseASCIIConversion(image)
+        if 's' in self.option:
+            self._saveConvertedImage(converted_image)
+        return converted_image
 
-    if "/" in image_file_path:
-        split_file_name = image_file_path.split('/')
-        image_name = split_file_name[-1]
-    else:
-        image_name = image_file_path
-    split_image_name = image_name.split('.')
-    file_name = split_image_name[0]
-    text_file_name = file_name + ".txt"
+    def _defaultConversion(self, image):
+        default_ascii_image = self.convert_image_to_ascii(image)
+        return default_ascii_image
 
-    with open(text_file_name, "w") as f:
-        f.write(image_ascii)
+    def _reverseASCIIConversion(self, image):
+        ''' Make method to convert image with reverse ASCII charactes '''
+        return self.convert_image_to_ascii(image)   # replace this with your reversed ascii image conversion
 
-def handle_image_conversion(image_filepath, arg=""):
-    image = None
-    try:
-        image = Image.open(image_filepath)
-    except Exception as e:
-        print(f"Unable to open image file {image_filepath}.")
-        print(e)
-        return
-
-    image_ascii = convert_image_to_ascii(image)
-    print(image_ascii)
-
-    if arg == "-s":
-        output_name = image_file_path.split('.')[0] + "_output.txt" 
+    def _createOutFilePath(self, file_name):
+        ''' Create out file path if not exists and return filepath '''
+        global OUT_FILE_PATH
+        if not os.path.exists(OUT_FILE_PATH):
+            os.mkdir(OUT_FILE_PATH)
+        file_path = OUT_FILE_PATH + file_name
+        return file_path
+    
+    def _saveConvertedImage(self, image):
+        ''' Save converted image as a text file '''
+        global OUT_FILE_PATH
+        partition_file_path = self.image_file_path.split('/')
+        output_file_name = partition_file_path[-1]
+        output_file_name = output_file_name.split('.')[0] + "_output.txt"
+        output_file_path = self._createOutFilePath(output_file_name)
         try:
-            f = open(output_name, "w")
-            f.write(image_ascii)
-            f.close
-            print(f"Image saved to -> {output_name}")
+            with open(output_file_path, 'w') as out_file:
+                out_file.write(image)
+            print(f"Image (as a text) saved to -> {output_file_path}")
         except:
-            print("An error occured!")
+            print("An error occured while saving image as a text file !")
             return False
+    
+    def handle_image_conversion(self):
+        image = None
+        try:
+            image = Image.open(self.image_file_path)
+        except Exception as e:
+            print(f"Unable to open image file {self.image_file_path}.")
+            print(e)
+            return
+
+        convertedImage = self.map_option_to_convert(image)
+        return convertedImage
+
 
 def check_file(f):
+    ''' Check file type, it should be png (as of now) '''
+    global HELP_MSG
     allowed_inputs_file = ["png"]
     try:
         if f.split('.')[-1] in allowed_inputs_file:
@@ -95,44 +131,26 @@ def check_file(f):
         else:
             return False
     except:
-        print(help_msg)
+        print(HELP_MSG)
         return False
 
-def check_inputs():
-    arguments = [x for x in sys.argv]
-    if 2 > len(arguments) or len(arguments) > 3 or check_file(arguments[-1]) == False:
-        print(help_msg)
-        return False
-    elif len(arguments) == 2 and check_file(arguments[-1]):
-        return ""
-    elif len(arguments) == 3 and arguments[1] == "-r" and check_file(arguments[-1]):
-        return arguments[1]
-    elif len(arguments) == 3 and arguments[1] == "-s" and check_file(arguments[-1]):
-        return arguments[1]
-    elif len(arguments) == 3 and arguments[1] == "-rs" and check_file(arguments[-1]):
-        return arguments[1]
+
+def get_cli_input():
+    ''' Check if program running through command line '''
+    cli_input = [inp for inp in sys.argv]
+    input_file = cli_input[-1]
+    input_option = '-c'
+    isFileTypeCorrect = check_file(input_file)
+    if len(cli_input) > 2:
+        input_option = cli_input[1]
+    return isFileTypeCorrect, input_file, input_option
 
 
 if __name__=='__main__':
-    import sys
-    todo = check_inputs()
-    ASCII_CHARS = [ '#', '?', '%', '.', 'S', '+', '.', '*', ':', ',', '@']
-
-    if todo == "":
-        image_file_path = sys.argv[1]
-        print(image_file_path)
-        handle_image_conversion(image_file_path)
-    elif todo == '-r':
-        ASCII_CHARS = [ '#', '?', '%', '.', 'S', '+', '.', '*', ':', ',', '@'][::-1]
-        image_file_path = sys.argv[2]
-        print(image_file_path)
-        handle_image_conversion(image_file_path)
-    elif todo == "-s":
-        image_file_path = sys.argv[2]
-        print(image_file_path)
-        handle_image_conversion(image_file_path, "-s")
-    elif todo == "-rs":
-        ASCII_CHARS = [ '#', '?', '%', '.', 'S', '+', '.', '*', ':', ',', '@'][::-1]
-        image_file_path = sys.argv[2]
-        print(image_file_path)
-        handle_image_conversion(image_file_path, "-s")
+    isFile, cli_input_file, cli_input_option = get_cli_input()
+    print(f"File {cli_input_file} is PNG ? {isFile}, called with option {cli_input_option} (-c is default)")
+    if not isFile:
+        print(HELP_MSG)
+        raise Exception(FILE_ERROR_MSG)
+    ascii_convert_obj = ConvertImageToASCII(file_path=cli_input_file, option=cli_input_option)
+    print(ascii_convert_obj.handle_image_conversion())
