b5c49d4d6c0733759f19f764c2049085f47715a9
adamstok <adamstokowski@outlook.com>
2020-10-04 20:00:55 +0200

    added tests without renaming the community-version file

--- tests/tests.py
+++ tests/tests.py
@@ -1,28 +1,27 @@
 import unittest
 import sys
 from PIL import Image
+import importlib
 sys.path.insert(1,'../')
-from community_version import handle_image_conversion, check_file 
+community_file = importlib.import_module('community-version', None)
 
 
 class Testing(unittest.TestCase):
 
-    def test_0_handle_bad_path(self):
-        test_path = 'bad path to input file'
-        def_output = handle_image_conversion(test_path)
-        self.assertIsNone(def_output)
+    def test_0_color_change(self):
+        def_output = community_file.color_change()
+        self.assertIs(def_output, "none")
 
-    def test_1_check_file(self):
-        file = "test.png"
-        def_output = True
-        self.assertTrue(def_output)
+    def test_1_check_inputs(self):
+        def_output = community_file.check_inputs()
+        help_msg = community_file.help_msg
+        self.assertIs(def_output, "")
 
-    def test_2_check_file(self):
-        file = "test.120"
-        def_output = False
-        self.assertFalse(def_output)
+    def test_2_colorText(self):
+        text = 'test'
+        def_output = community_file.colorText(text)
+        self.assertIs(type(def_output), str)
 
 
 if __name__ == '__main__':
     unittest.main()
-
