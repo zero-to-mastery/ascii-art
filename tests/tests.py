15368a92b840543055892b2d54dd54d3d0b33e65
adamstok <adamstokowski@outlook.com>
2020-10-05 13:19:53 +0200

    updating the tests - just have time to make it one

--- tests/tests.py
+++ tests/tests.py
@@ -8,16 +8,7 @@ community_file = importlib.import_module('community-version', None)
 
 class Testing(unittest.TestCase):
 
-    def test_0_color_change(self):
-        def_output = community_file.color_change()
-        self.assertIs(def_output, "none")
-
-    def test_1_check_inputs(self):
-        def_output = community_file.check_inputs()
-        help_msg = community_file.help_msg
-        self.assertIs(def_output, "")
-
-    def test_2_colorText(self):
+    def test_1_colorText(self):
         text = 'test'
         def_output = community_file.colorText(text)
         self.assertIs(type(def_output), str)
