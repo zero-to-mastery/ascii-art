48bb32425c844084ce99affc87ed7abf2f2ca940
weinlincheng <william.cheng625@gmail.com>
2020-10-07 17:40:05 +0800

    Times 0.5 to new_height so that the output ascii ratio looks like original image

--- community-version.py
+++ community-version.py
@@ -59,7 +59,7 @@ def scale_image(image, new_width=100):
     """
     (original_width, original_height) = image.size
     aspect_ratio = original_height / float(original_width)
-    new_height = int(aspect_ratio * new_width)
+    new_height = int(aspect_ratio * new_width * 0.5)
 
     new_image = image.resize((new_width, new_height))
     return new_image
