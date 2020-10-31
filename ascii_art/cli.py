253f5fdc4712beac502436984268300eb0649c34
Szabó Péter <1254135+szabopeter@users.noreply.github.com>
2020-10-31 14:48:20 +0100

    CLI: turn switches to commands
    Should fix #88

--- ascii_art/cli.py
+++ ascii_art/cli.py
@@ -6,7 +6,12 @@ from ascii_art.ascii_art import (all_supported_files, ascii_textinput, process, 
                                  COLOR_OPTIONS, SUPPORTED_IMAGE_TYPES, ascii_text)
 
 
-@click.command()
+@click.group()
+def main():
+    pass
+
+
+@main.command()
 @click.argument('input_files', type=click.Path(exists=True), nargs=-1)
 @click.option('-r', '--reverse', is_flag=True, help='reverse the ASCII_CHARS')
 @click.option('-s', '--save', is_flag=True,
@@ -15,45 +20,48 @@ from ascii_art.ascii_art import (all_supported_files, ascii_textinput, process, 
               help='Specify the name of the output file instead of using the default. -s is implied.')
 @click.option('-w', '--width', default=100, type=int,
               help='scale the image to fit a custom width')
-@click.option('--credits', is_flag=True, help="Show credits")
-@click.option('--clock', is_flag=True,
-              help='show clock as a colorful animation. resize the terminal or press "q" or "x" to exit the clock.')
 @click.option('--all', is_flag=True, help='convert all supported files')
 @click.option('-c', '--color', type=click.Choice(COLOR_OPTIONS, case_sensitive=False), default='black',
               help='Set output color')
-@click.option('--textinput', is_flag=True, help='Convert Simple Text Into Ascii Text Format, Enter Text After Prompt')
-@click.option('--text', default=None, type=str, help='Convert Simple Text Into Ascii Text Format')
-@click.option('--types', is_flag=True, help='list supported image formats and exit.')
 @click.option('-hr', '--highres', is_flag=True, help='Converts using a wide range of Ascii characters.')
-def main(input_files, reverse, save, output, width, credits, clock, all, color, textinput, text, types, highres):
+def process(input_files, reverse, save, output, width, all, color, highres):
     """
-    Converts an image to a text file, using ascii characters to display it.
+    Converts images to text files, using ascii characters to display it.
     """
-    if types:
-        print(', '.join(SUPPORTED_IMAGE_TYPES))
-        return
-    if clock:
-        show_clock()
-        return
-    if credits:
-        show_credits()
-        print()
-
     if all:
         input_files = all_supported_files()
-
-    if textinput:
-        ascii_textinput()
-        return
-
-    if text:
-        ascii_text(text)
 
     for file in input_files:
         process(file, reverse=reverse, save=save,
                 output=output, width=width, color=color.lower(), highres=highres)
     if not input_files and len(sys.argv) == 1:
         print("Image not specified. Please specify image or add --help for help.")
+
+
+@main.command(help='list supported image formats and exit.')
+def types():
+    print(', '.join(SUPPORTED_IMAGE_TYPES))
+
+
+@main.command(help="Show credits")
+def credits():
+    show_credits()
+
+@main.command(help='show clock as a colorful animation. resize the terminal or press "q" or "x" to exit the clock.')
+def clock():
+    show_clock()
+
+
+@main.command(help='Convert simple text into ASCII text format')
+@click.argument("value", type=str, default="ASCII ART")
+def text(value):
+    ascii_text(value)
+
+
+@main.command(help='Convert simple text into ASCII text format, enter text after prompt')
+def textinput():
+    ascii_textinput()
+
 
 
 if __name__ == '__main__':
