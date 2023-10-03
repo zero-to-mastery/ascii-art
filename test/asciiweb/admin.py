b5128cc2854e8af631a1e7bb3518e605af231d80
Dennismwendwa <dennismusembi2@gmail.com>
2023-10-03 03:57:54 +0300

    deleted test

--- test/asciiweb/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import RawImage
-
-admin.site.register(RawImage)
--- test/asciiweb/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "asciiweb"
-
-urlpatterns = [
-        path("", views.home, name="home"),
-        ]
