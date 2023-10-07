a6992fbb55060a178d84ba6e9d8bca9677afe18c
Kennith Nichol <ken.nichol@live.ca>
2023-10-07 15:22:12 +0000

    import community version so we don't duplicate code

--- ascii_art_web_app/app.py
+++ ascii_art_web_app/app.py
@@ -1,7 +1,20 @@
 from flask import Flask, render_template, request
 from PIL import Image
+from inspect import getsourcefile
 import io
-from convert_image import convert_image_to_ascii
+import sys
+import os.path as path
+import importlib
+
+# ugly: @todo change this whole app to work from the same pacakge
+dir = path.dirname(path.abspath(getsourcefile(lambda:0)))
+sys.path.insert(0, dir[:dir.rfind(path.sep)])
+core = importlib.import_module("community-version")
+sys.path.pop(0)
+
+#from community-version import convert_image_to_ascii
+# from convert_image import convert_image_to_ascii
+
 
 app = Flask(__name__)
 
@@ -12,10 +25,10 @@ def index():
         image = request.files["image"]
         if image:
             image = Image.open(io.BytesIO(image.read()))
-            ascii_art = convert_image_to_ascii(image)
+            ascii_art = core.convert_image_to_ascii(image)
             return render_template("index.html", ascii_art=ascii_art)
 
     return render_template("index.html", ascii_art=None)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
