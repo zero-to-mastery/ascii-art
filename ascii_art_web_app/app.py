3b9357ab215f195ac1cfe54669a3f95976397ffd
sravanigodavarthi <lakshmisravani1895@gmail.com>
2023-10-23 18:58:35 -0400

    Enhance UI to include original image and ASCII art,with scroll support

--- ascii_art_web_app/app.py
+++ ascii_art_web_app/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from PIL import Image
 from inspect import getsourcefile
 import io
+import base64
 import sys
 import os.path as path
 import importlib
@@ -16,18 +17,25 @@ sys.path.pop(0)
 # from convert_image import convert_image_to_ascii
 
 app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template("index.html")
 
 @app.route("/", methods=["GET", "POST"])
-def index():
+def image_render():
     if request.method == "POST":
         # Handle image upload
         image = request.files["image"]
         if image:
             image = Image.open(io.BytesIO(image.read()))
+            # Convert the image to a base64 string for HTML display
+            buffered = io.BytesIO()
+            image.save(buffered, format="PNG")
+            img_str = base64.b64encode(buffered.getvalue()).decode()
+            # Convert image to ascii art
             ascii_art = core.convert_image_to_ascii(image)
-            return render_template("index.html", ascii_art=ascii_art)
-
-    return render_template("index.html", ascii_art=None)
+            return render_template("ascii_art.html",ascii_art=ascii_art,image_str=img_str)
+    return render_template("ascii_art.html",ascii_art=None,image_str=None)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8080)
