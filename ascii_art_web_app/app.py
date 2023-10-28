c577036b7fb0e01fb458cf610ff564533e081e93
Anthony <Anteemony@gmail.com>
2023-10-28 21:30:22 +0100

    add holiday season title

--- ascii_art_web_app/app.py
+++ ascii_art_web_app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from PIL import Image
 from inspect import getsourcefile
+from datetime import datetime
 import io
 import sys
 import os.path as path
@@ -19,15 +20,22 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    season_emoji = None
+    # Set emoji for the holiday season
+    seasons = {"April":"🐰", "October":"🎃", "December":"🎅"}
+    if datetime.now().strftime("%B") in seasons:
+        season_emoji = seasons[datetime.now().strftime("%B")]
+
     if request.method == "POST":
         # Handle image upload
         image = request.files["image"]
         if image:
             image = Image.open(io.BytesIO(image.read()))
             ascii_art = core.convert_image_to_ascii(image)
-            return render_template("index.html", ascii_art=ascii_art)
 
-    return render_template("index.html", ascii_art=None)
+            return render_template("index.html", ascii_art=ascii_art, season_emoji=season_emoji)
+
+    return render_template("index.html", ascii_art=None, season_emoji=season_emoji)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8080)
