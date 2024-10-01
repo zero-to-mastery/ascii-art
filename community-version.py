b2813df77fcdcc9e11b5d9ae7d3dc087b46b7b9b
Laura White <lmwhite7676@gmail.com>
2024-10-01 11:36:04 -0500

    updates community-version.py

--- community-version.py
+++ community-version.py
@@ -1,59 +1,49 @@
-## Community Version
-
 # this project requires Pillow installation: https://pillow.readthedocs.io/en/stable/installation.html
 
-# code credit goes to: https://www.hackerearth.com/practice/notes/beautiful-python-a-simple-ascii-art-generator-from-images/
-# code modified to work with Python 3 by @aneagoie
+#code credit goes to: https://www.hackerearth.com/practice/notes/beautiful-python-a-simple-ascii-art-generator-from-images/
+#code modified to work with Python 3 by @aneagoie
 from PIL import Image
+ASCII_CHARS = [ '#', '?', '%', '.', 'S', '+', '.', '*', ':', ',', '@']
 
-ASCII_CHARS = ["#", "?", "%", ".", "S", "+", ".", "*", ":", ",", "@"]
-
-
-def scale_image(image: Image.Image, new_width: int = 100) -> Image.Image:
-    """Resizes an image preserving the aspect ratio."""
+def scale_image(image, new_width=100):
+    """Resizes an image preserving the aspect ratio.
+    """
     (original_width, original_height) = image.size
-    aspect_ratio = original_height / float(original_width)
+    aspect_ratio = original_height/float(original_width)
     new_height = int(aspect_ratio * new_width)
 
     new_image = image.resize((new_width, new_height))
     return new_image
 
+def convert_to_grayscale(image):
+    return image.convert('L')
 
-def convert_to_grayscale(image: Image.Image) -> Image.Image:
-    return image.convert("L")
-
-
-def map_pixels_to_ascii_chars(image: Image.Image, range_width: int = 25) -> str:
+def map_pixels_to_ascii_chars(image, range_width=25):
     """Maps each pixel to an ascii char based on the range
     in which it lies.
 
     0-255 is divided into 11 ranges of 25 pixels each.
     """
 
-    pixels_in_image = list(image.getdata())  # type: ignore
-    pixels_to_chars = [
-        ASCII_CHARS[int(pixel_value / range_width)] for pixel_value in pixels_in_image
-    ]
+    pixels_in_image = list(image.getdata())
+    pixels_to_chars = [ASCII_CHARS[int(pixel_value/range_width)] for pixel_value in
+            pixels_in_image]
 
     return "".join(pixels_to_chars)
 
-
-def convert_image_to_ascii(image: Image.Image, new_width: int = 100) -> str:
+def convert_image_to_ascii(image, new_width=100):
     image = scale_image(image)
     image = convert_to_grayscale(image)
 
     pixels_to_chars = map_pixels_to_ascii_chars(image)
     len_pixels_to_chars = len(pixels_to_chars)
 
-    image_ascii = [
-        pixels_to_chars[index : index + new_width]
-        for index in range(0, len_pixels_to_chars, new_width)
-    ]
+    image_ascii = [pixels_to_chars[index: index + new_width] for index in
+            range(0, len_pixels_to_chars, new_width)]
 
     return "\n".join(image_ascii)
 
-
-def handle_image_conversion(image_filepath: str) -> None:
+def handle_image_conversion(image_filepath):
     image = None
     try:
         image = Image.open(image_filepath)
@@ -65,15 +55,9 @@ def handle_image_conversion(image_filepath: str) -> None:
     image_ascii = convert_image_to_ascii(image)
     print(image_ascii)
 
-
-if __name__ == "__main__":
+if __name__=='__main__':
     import sys
-
-    if len(sys.argv) < 2:
-        print("Usage: python script.py <image_file_path>")
-        sys.exit(1)
 
     image_file_path = sys.argv[1]
     print(image_file_path)
     handle_image_conversion(image_file_path)
-
