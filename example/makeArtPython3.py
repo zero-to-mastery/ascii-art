9a083c5ee95bc8775e6b061fa08e175838e2c1f4
Saketh Yalamanchili <saketh.engineer@gmail.com>
2024-10-02 11:38:57 +0530

    Updated makeArtPython3.py to work with Python 3

--- example/makeArtPython3.py
+++ example/makeArtPython3.py
@@ -39,7 +39,7 @@ def convert_image_to_ascii(image, new_width=100):
     len_pixels_to_chars = len(pixels_to_chars)
 
     image_ascii = [pixels_to_chars[index: index + new_width] for index in
-            xrange(0, len_pixels_to_chars, new_width)]
+            range(0, len_pixels_to_chars, new_width)]
 
     return "\n".join(image_ascii)
 
@@ -47,13 +47,13 @@ def handle_image_conversion(image_filepath):
     image = None
     try:
         image = Image.open(image_filepath)
-    except Exception, e:
-        print "Unable to open image file {image_filepath}.".format(image_filepath=image_filepath)
-        print e
+    except Exception as e:
+        print("Unable to open image file {image_filepath}.".format(image_filepath=image_filepath))
+        print(e)
         return
 
     image_ascii = convert_image_to_ascii(image)
-    print image_ascii
+    print(image_ascii)
 
 if __name__=='__main__':
     import sys
